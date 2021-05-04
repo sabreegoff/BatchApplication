72282b79a268265e6c6834b8c2538c2fc21db8cb
SaBree Goff <SGoff@sorenson.com>
2021-05-03 22:17:46 -0700

    Add project files.

---
+++ BatchApplication/Mixtape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BatchApplication
+{
+	public class Mixtape
+	{
+		public List<User> users { get; set; }
+		public List<Playlist> playlists { get; set; }
+		public List<Song> songs { get; set; }
+	}
+
+	public class User
+	{
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+		public string change_action { get; set; }
+		public string id { get; set; }
+		public string name { get; set; }
+	}
+
+	public class Playlist
+	{
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+		public string change_action { get; set; }
+		public string id { get; set; }
+		public string user_id { get; set; }
+		
+		public string[] song_ids { get; set; }
+	}
+
+	public class Song
+	{
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+		public string change_action { get; set; }
+		public string id { get; set; }
+		public string artist { get; set; }
+		public string title { get; set; }
+	}
+}
---
+++ BatchApplication/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace BatchApplication
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			string providedMixtape = "https://gist.githubusercontent.com/jmodjeska/0679cf6cd670f76f07f1874ce00daaeb/raw/a4ac53fa86452ac26d706df2e851fb7d02697b4b/mixtape-data.json";
+			string changeList = "C:\\Users\\SGoff\\source\\repos\\BatchApplication\\BatchApplication\\Changes.json";
+
+			Mixtape mixtape = ReadMixtape(providedMixtape);
+			Mixtape mixtapeWithChages = ReadAndApplyChanges(mixtape, changeList);
+			string jsonString = SerializeJson(mixtapeWithChages);
+
+			Console.WriteLine(jsonString);
+		}
+
+		public static Mixtape ReadMixtape(string url)
+		{
+			try
+			{
+				using (WebClient wc = new WebClient())
+				{
+					string jsonString = wc.DownloadString(url);
+
+					Mixtape mixtape = JsonSerializer.Deserialize<Mixtape>(jsonString);
+
+					return mixtape;
+				};
+			}
+			catch(Exception ex)
+			{
+				throw new Exception($"Unable to parse Json from {url}", ex);
+			}
+		}
+
+		public static Mixtape ReadAndApplyChanges(Mixtape currentMixtape, string changes)
+		{
+			Mixtape updatedMixtape = currentMixtape;
+			Mixtape changesMixtape = ReadMixtape(changes);
+
+			foreach(User u in changesMixtape.users)
+			{
+				if(u.change_action == "update")
+				{
+					//use linq so we arent doing this manually
+					foreach(User us in updatedMixtape.users)
+					{
+						if(u.id == us.id)
+						{
+							us.name = u.name;
+						}
+					}
+				}
+
+				if (u.change_action == "add")
+				{
+					if (!updatedMixtape.users.Any(user => user.id == u.id))
+					{
+						updatedMixtape.users.Add(u);
+					}
+				}
+
+				if (u.change_action == "delete")
+				{
+					if (updatedMixtape.users.Any(user => user.id == u.id))
+					{
+						User removeUser = updatedMixtape.users.Where(us => us.id == u.id).First();
+						updatedMixtape.users.Remove(removeUser);
+					}
+				}
+			}
+
+			return updatedMixtape;
+		}
+
+		public static string SerializeJson(Mixtape objectToJson)
+		{
+			string jsonString = JsonSerializer.Serialize(objectToJson);
+
+			return jsonString;
+
+		}
+	}
+}
+
